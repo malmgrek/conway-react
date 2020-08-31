8db1de4bacf4f58b63b7a54a766fc50664df3835
Stratos Staboulis <stratos.staboulis@leanheat.com>
2020-08-31 20:53:10 +0300

    Updates

---
+++ precache-manifest.1675079e83d88af2c7f1e1a694029d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad398e3391ea89a35289309aa6069b7",
+    "url": "/conway-react/index.html"
+  },
+  {
+    "revision": "37464a911a05e26e263b",
+    "url": "/conway-react/static/css/main.8802b175.chunk.css"
+  },
+  {
+    "revision": "99c0e9cd672ddc8406f6",
+    "url": "/conway-react/static/js/2.26d3b186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conway-react/static/js/2.26d3b186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37464a911a05e26e263b",
+    "url": "/conway-react/static/js/main.a9adec0c.chunk.js"
+  },
+  {
+    "revision": "a2b7fc3ef4cf5ecf795f",
+    "url": "/conway-react/static/js/runtime-main.47e95d86.js"
+  }
+]);
